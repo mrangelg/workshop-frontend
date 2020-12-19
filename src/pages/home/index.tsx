b008b6335a2d952a14fad9928d15f4dbd26bed0b
Madelin Rangel Grisales <mrangel@utp.edu.co>
2020-12-19 11:29:38 -0500

    consumption service get numbers of word and lines

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,6 +9,11 @@ interface Song {
   lyrics: string;
 }
 
+interface NumberOfWordsAndLines {
+  words: string;
+  lines: string;
+}
+
 type FormData = {
   artist: string;
   song: string;
@@ -18,6 +23,8 @@ function Home(): JSX.Element {
   const [artistName, setArtistName] = useState<string>();
   const [songName, setSongName] = useState<string>();
   const [lyric, setLyric] = useState<Song>();
+  const [numberOfWords, setNumberOfWords] = useState<string>();
+  const [numberOfLines, setNumberOfLines] = useState<string>();
 
   const { register, handleSubmit, watch, errors } = useForm<FormData>();
 
@@ -34,15 +41,32 @@ function Home(): JSX.Element {
         console.log('axios: ', response.data);
         if (response.data) {
           setLyric({ lyrics: response.data.lyrics });
-        } else {
-          setLyric({ lyrics: '' });
         }
       })
       .catch((error) => {
         console.log(error.response.data.error);
-        setLyric({ lyrics: '' });
       });
   }, [artistName, songName]);
+
+  useEffect(() => {
+    if (lyric) {
+      axios
+        .get<NumberOfWordsAndLines>(
+          `https://localhost:4000/number-words-lines/${lyric.lyrics}`,
+        )
+        .then((response) => {
+          console.log('axios: ', response.data);
+          if (response.data) {
+            setNumberOfWords(response.data.words);
+            setNumberOfLines(response.data.lines);
+          } else {
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [lyric]);
 
   console.log('lyric: ', lyric);
   return (
@@ -50,9 +74,11 @@ function Home(): JSX.Element {
       <form onSubmit={handleSubmit(onSubmit)}>
         <Input name="artist" register={register} label="Artist" />
         <Input name="song" register={register} label="Song" />
-        <Button name="Search" />
+        <Button name="Find lyric!" />
       </form>
-      <div>{lyric?.lyrics}</div>
+      {lyric?.lyrics && <div>{lyric?.lyrics}</div>}
+      {numberOfWords && <div>Number of lines: {numberOfWords}</div>}
+      {numberOfLines && <div>Number of lines: {numberOfLines}</div>}
     </Container>
   );
 }

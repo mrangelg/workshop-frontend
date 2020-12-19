43543519a8b83b107dfdbe4e3d080d5d7202fe07
Madelin Rangel Grisales <mrangel@utp.edu.co>
2020-12-19 05:53:16 -0500

    Axios dependencie installed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import Gretting from '../../components/greeting';
 
+interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type?: string;
+  gender: string;
+  origin: URL;
+  location: URL;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+type URL = {
+  name: string;
+  url: string;
+};
+
 function Home(): JSX.Element {
+  useEffect(() => {
+    axios
+      .get<Character>('https://rickandmortyapi.com/api/character/2')
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
+
   return <Gretting />;
 }
 
